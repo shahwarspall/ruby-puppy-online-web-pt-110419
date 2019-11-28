166ea739cf3b095b8a15b1326bcebd83bbe3e268
Shahwar Spall <shevy.spall@gmail.com>
2019-11-28 22:02:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
-# Add your code here
+require "pry"
+class Dog 
+  
+  @@all =[]
+  
+  def initialize(name)
+    @name= name
+    @@all << self 
+  end
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all 
+    
+    binding.pry 
+    
+  end 
+end 
